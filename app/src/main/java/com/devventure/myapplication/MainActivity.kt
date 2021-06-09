779f3e61b7cfed229e898e3b440028bc6900b6e0
Joao Felipe <brjoaof@gmail.com>
2021-06-09 20:56:38 -0300

    Segunda Entrega

--- app/src/main/java/com/devventure/myapplication/MainActivity.kt
+++ app/src/main/java/com/devventure/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.devventure.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,19 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dado1 = findViewById<TextView>(R.id.Dado1_TV)
-        val dado2 = findViewById<TextView>(R.id.Dado2_TV)
+        val dado1 = findViewById<ImageView>(R.id.dado1_IV)
+        val dado2 = findViewById<ImageView>(R.id.dado2_IV)
         val btn = findViewById<Button>(R.id.throw_btn)
 
+        val listImages = listOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6,
+        )
+
         btn.setOnClickListener{
-            dado1.text = getRandomNumber().toString();
-            dado2.text = getRandomNumber().toString();
+            dado1.setImageResource(listImages.random())
+            dado2.setImageResource(listImages.random())
         }
 
-    }
-
-    private fun getRandomNumber() : Int {
-        return (1..6).random()
     }
 
 }
